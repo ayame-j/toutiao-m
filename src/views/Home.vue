18bb8a02653232ebf55e79024dd453e46d1f59d6
jiangqi <769961115@qq.com>
2021-01-17 17:16:59 +0800

    11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="home-container">
-    <van-nav-bar class="page-nav-bar"
-      ><van-button class="search-btn" slot="title" type="info" size="small" round icon="search"
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        class="search-btn"
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
         >搜索</van-button
-      ></van-nav-bar
-    >
+      >
+    </van-nav-bar>
+    <van-tabs class="channle-tabs" slot="nav-left" scrollspy v-model="active" animated swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <div slot="nav-right" class="hamburger-btn">
+        <i class="iconfont icongengduo"></i>
+      </div>
+      <div slot="nav-right" placeholder=""></div>
+    </van-tabs>
   </div>
 </template>
 
@@ -14,25 +30,68 @@ export default {
   components: {},
   props: {},
   data() {
-    return {};
+    return { active: 1 };
   },
 };
 </script>
 
 <style lang="less" scoped>
-  .home-container{
-    .van-nav-bar__title{
-      max-width: unset;
+.home-container {
+  /deep/ .van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 35px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
     }
-    .search-btn{
-      width: 555px;
-      height: 64px;
-      background-color: #5babfb;
-      border: none;
-      font-size: 28px;
-      .van-icon{
-        font-size: 32px;
+  }
+  /deep/.channle-tabs{
+    .van-tabs__wrap{
+      height: 82px;
+    }
+    .van-tab{
+      width: 200px;
+      border-right: 1px solid #edeff3;
+      font-size: 30px;
+      color: #777;
+      line-height: normal;
+    }
+    .van-tab--active{
+      color: #333;
+    }
+    .van-tabs__line{
+      width: 31px !important;
+      height: 6px;
+      background-color: #3296fa;
+    }
+    .hamburger-btn{
+      position: fixed;
+      right: 0;
+      width: 66px;
+      height: 80px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #fff;
+      opacity: 0.9;
+      i.iconfont{
+        font-size: 33px;
+      }
+      &:before {
+        content:"";
+        position: absolute;
+        left:0;
+        width:1px;
+        height: 100%;
+        background-image: url(~@/assets/gradient-gray-line.png);
+        background-size: contain;
       }
     }
   }
-</style>>
+}
+</style>
